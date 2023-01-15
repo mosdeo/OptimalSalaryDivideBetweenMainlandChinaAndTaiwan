9a0c4cc09718344110b8ec8d0d06637acbcf84ac
Lin, Kao-Yuan <mosdeo@gmail.com>
2023-01-15 19:14:53 +0800

    獨立出五險一金的計算

--- mainlandchina_tax_algo.py
+++ mainlandchina_tax_algo.py
@@ -16,12 +16,7 @@ class MainlandChinaTax:
     def __init__(self, salary):
         self.salary = salary
 
-    def tax(self):
-        # 參考
-        # 社保缴费怎么计算，医保看病能报多少？3分钟带你读懂社保 - 塞涅卡的文章 - 知乎
-        # https://zhuanlan.zhihu.com/p/43131487
-
-        start_tax_level = 5000 # 起徵點
+    def five_insurances(self):
         retirement = self.salary * 0.08 # 退休金
         medical_insurance = self.salary * 0.02 # 醫療保險
         unemployment_insurance = self.salary * 0.005 # 失業保險
@@ -31,17 +26,27 @@ class MainlandChinaTax:
         # 工伤保险的参险费率叫什么？ - 愿醉的回答 - 知乎
         # https://www.zhihu.com/question/366265079/answer/976702720
         work_injury_insurance = self.salary * 0.002
+        return np.sum([
+            retirement, 
+            medical_insurance, 
+            unemployment_insurance, 
+            maternity_insurance, 
+            work_injury_insurance])
 
-        house_savings = self.salary * 0.12 # 住房公積金
+    def house_savings(self, rate=0.05):
+        return self.salary * rate
+
+    def tax(self):
+        # 參考
+        # 社保缴费怎么计算，医保看病能报多少？3分钟带你读懂社保 - 塞涅卡的文章 - 知乎
+        # https://zhuanlan.zhihu.com/p/43131487
+
+        start_tax_level = 5000 # 起徵點
 
         tax_free_item = [
             start_tax_level,
-            retirement,
-            medical_insurance,
-            unemployment_insurance,
-            maternity_insurance,
-            work_injury_insurance,
-            house_savings,
+            self.five_insurances(),
+            self.house_savings(),
         ]
 
         tax_salary = self.salary - np.sum(tax_free_item) # 稅前薪資 - 免稅項目
@@ -52,8 +57,7 @@ class MainlandChinaTax:
             if min_ < tax_salary <= max_:
                 tax = tax_salary * rate - base
                 break
-
         return tax
 
     def salary_after_tax(self):
-        return self.salary - self.tax()
+        return self.salary - self.tax() - self.five_insurances() - self.house_savings()
