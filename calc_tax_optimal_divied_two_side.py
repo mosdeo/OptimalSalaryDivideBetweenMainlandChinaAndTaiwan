18274adb0a39d3de29e3b5ddd3367d66db5169c1
Lin, Kao-Yuan <mosdeo@gmail.com>
2023-01-18 14:13:28 +0800

    修改輸出格式與計算方式

--- calc_tax_optimal_divied_two_side.py
+++ calc_tax_optimal_divied_two_side.py
@@ -16,14 +16,17 @@ if __name__ == '__main__':
     for s in range(salary):
         # 期望薪資TWD部分、扣項、到手部分
         salary_TWD = s * exchange_rate_CNY_to_TWD
-        deductions_TWD = salary_TWD - TaiwanTax(salary_TWD).net_salary()
+        taiwanTax = TaiwanTax(salary_TWD)
+        deductions_TWD = salary_TWD - taiwanTax.net_salary()
         net_salary_TWD = salary_TWD - deductions_TWD
 
         # 期望薪資CNY部分、扣項、到手部分
         # 這裡將公積金算入到手薪資中，但依然是扣項
+        # 公積金=12%, 不繳失業保險、不繳退休金
         salary_CNY = salary - s
-        deductions_CNY = salary_CNY - MainlandChinaTax(salary_CNY).salary_after_tax()
-        salary_after_tax_CNY = salary_CNY - deductions_CNY + MainlandChinaTax(salary_CNY).house_savings() * 2
+        mainlandChinaTax = MainlandChinaTax(salary_CNY, house_savings_rate=0.12, is_retirement=False, is_unemployment_insurance=False)
+        deductions_CNY = salary_CNY - mainlandChinaTax.salary_after_tax()
+        salary_after_tax_CNY = salary_CNY - deductions_CNY + mainlandChinaTax.house_savings() * 2
 
         # 期望總薪資的到手薪資(CNY計)
         net_salary = net_salary_TWD/exchange_rate_CNY_to_TWD + salary_after_tax_CNY
@@ -34,17 +37,17 @@ if __name__ == '__main__':
         table[s, 2] = date # "匯率時間",
 
         table[s, 3] = salary_TWD # "在台領薪資(TWD)",
-        table[s, 4] = TaiwanTax(salary_TWD).labor_insurance_fee() # "個人繳勞保費",
-        table[s, 5] = TaiwanTax(salary_TWD).health_insurance_fee()  # "個人繳健保費",
-        table[s, 6] = TaiwanTax(salary_TWD).tax() # "台灣税金",
+        table[s, 4] = taiwanTax.labor_insurance_fee() # "個人繳勞保費",
+        table[s, 5] = taiwanTax.health_insurance_fee()  # "個人繳健保費",
+        table[s, 6] = taiwanTax.tax() # "台灣税金",
 
         table[s, 7] = salary_CNY # "在陸領薪資(CNY)",
-        table[s, 8] = MainlandChinaTax(salary_CNY).medical_insurance() # "個人繳醫保費",
-        table[s, 9] = MainlandChinaTax(salary_CNY).maternity_insurance() # "個人繳生育保險費",
-        table[s,10] = MainlandChinaTax(salary_CNY).unemployment_insurance() # "個人繳失業保險費",
-        table[s,11] = MainlandChinaTax(salary_CNY).work_injury_insurance() # "個人繳工傷保險費",
-        table[s,12] = MainlandChinaTax(salary_CNY).retirement() # "個人繳養老金",
-        table[s,13] = MainlandChinaTax(salary_CNY).house_savings() # "個人繳公積金",
+        table[s, 8] = mainlandChinaTax.medical_insurance() # "個人繳醫保費",
+        table[s, 9] = mainlandChinaTax.maternity_insurance() # "個人繳生育保險費",
+        table[s,10] = mainlandChinaTax.unemployment_insurance() # "個人繳失業保險費",
+        table[s,11] = mainlandChinaTax.work_injury_insurance() # "個人繳工傷保險費",
+        table[s,12] = mainlandChinaTax.retirement() # "個人繳養老金",
+        table[s,13] = mainlandChinaTax.house_savings() # "個人繳公積金",
 
         table[s,14] = net_salary # "到手總和(CNY)(計入勞資雙方公積金)",
         table[s,15] = net_salary/salary # "到手比率(%)",
@@ -61,9 +64,9 @@ if __name__ == '__main__':
 
     # 印出最佳分配
     print('最佳分配: ')
-    print('在台領薪資(TWD): {}'.format(table[optimal_divide_index, 3]))
-    print('在陸領薪資(CNY): {}'.format(table[optimal_divide_index, 7]))
-    print('申報薪資比率(台:陸): {}'.format((float(table[optimal_divide_index, 3])/exchange_rate_CNY_to_TWD)/table[optimal_divide_index, 7]))
-    print('到手總和(CNY)(計入勞資雙方公積金): {}'.format(table[optimal_divide_index, 14]))
-    print('到手比率(%): {}'.format(table[optimal_divide_index, 15]))
+    print('- 在台領薪資(TWD): {}'.format(table[optimal_divide_index, 3]))
+    print('- 在陸領薪資(CNY): {}'.format(table[optimal_divide_index, 7]))
+    print('- 申報薪資比率(台:陸): {}'.format((float(table[optimal_divide_index, 3])/exchange_rate_CNY_to_TWD)/table[optimal_divide_index, 7]))
+    print('- 到手總和(CNY)(計入勞資雙方公積金): {}'.format(table[optimal_divide_index, 14]))
+    print('- 到手比率(%): {}'.format(table[optimal_divide_index, 15]))
     
